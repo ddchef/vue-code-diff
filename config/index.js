a1bbe3ccdaa00f0ca39907c2a0b5d484474e4d10
ddchef <o.o@xjie.me>
2018-06-01 16:02:50 +0800

    修改(webpack)：打包方式

--- config/index.js
+++ config/index.js
@@ -72,5 +72,36 @@ module.exports = {
     // `npm run build --report`
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
+  },
+
+  demo: {
+    // Template for index.html
+    index: path.resolve(__dirname, '../demo/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../demo'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: true,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
   }
 }
