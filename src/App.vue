27594027b1be01c09064016d26f3fc77656bcf00
ddchef <o.o@xjie.me>
2018-06-01 13:57:54 +0800

    完善demo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-    <code-diff :old-string="oldStr" :new-string="newStr" :context="10" />
+    <el-form>
+      <el-row :gutter="10">
+        <el-col :span="12">
+          <el-form-item label="旧数据：">
+            <el-input v-model="oldStr" type="textarea" :autosize="{minRows: 2, maxRows: 15}" placeholder="请输入旧数据"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="新数据：">
+            <el-input v-model="newStr" type="textarea" :autosize="{minRows: 2, maxRows: 15}" placeholder="请输入新数据"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="展示效果：">
+            <el-switch v-model="fotmat" active-text="line-by-line" inactive-text="side-by-side"></el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="差异化范围：">
+            <el-input-number v-model="context" placeholder=""></el-input-number>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form>
+    <code-diff :old-string="oldStr" :new-string="newStr" :context="context" :output-format="outputFormat" />
   </div>
 </template>
 
 <script>
-import codeDiff from '../dist/vue-code-diff'
+import codeDiff from './lib/index.js'
 import oldStr from './date/oldStr'
 import newStr from './date/newStr'
 export default {
@@ -16,7 +40,14 @@ export default {
   data () {
     return {
       oldStr,
-      newStr
+      newStr,
+      fotmat: false,
+      context: 5
+    }
+  },
+  computed: {
+    outputFormat () {
+      return this.fotmat ? 'line-by-line' : 'side-by-side'
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
