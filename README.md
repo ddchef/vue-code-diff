419d86646d15102218781861440b7bb08c01bb17
shangguanyanpeng <shangguanyanpeng@360.cn>
2020-06-24 12:08:16 +0800

    增加参数值

--- README.md
+++ README.md
@@ -35,6 +35,11 @@ export default {
 | new-string| 新的字符串| string  |   —    |    —     |
 | context| 不同地方上下间隔多少行不隐藏 | number  |   —    |    —     |
 | outputFormat| 展示的方式 | string  |   line-by-line，side-by-side    |    line-by-line     |
+| drawFileList | 展示对比文件列表 | boolean | - | false |
+| renderNothingWhenEmpty | 当无对比时不渲染 | boolean | - | false |
+| diffStyle | 每行中对比差异级别 | string | word, char | word |
+| fileName | 文件名 | string | - |  |
+| isShowNoChange | 当无对比时展示源代码 | boolean | - | false |
 
 
 ## 效果展示
--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,31 @@
             <el-button type="text" @click="handleClearLocalStorage">清除</el-button>
           </el-form-item>
         </el-col>
+        <el-col :span="8">
+          <el-form-item label="展示对比文件列表">
+            <el-switch v-model="drawFileList" @click="drawFileList = !drawFileList"></el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="noDiff不渲染">
+            <el-switch v-model="renderNothingWhenEmpty" @click="renderNothingWhenEmpty = !renderNothingWhenEmpty"></el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="noDiff显示源代码">
+            <el-switch v-model="isShowNoChange" @click="isShowNoChange = !isShowNoChange"></el-switch>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="差异级别">
+            <el-button v-model="diffStyle" @click="diffStyle = diffStyle === 'char' ? 'word' : 'char'">{{diffStyle}}</el-button>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="文件名">
+            <el-input v-model="fileName"></el-input>
+          </el-form-item>
+        </el-col>
       </el-row>
     </el-form>
     <code-diff
@@ -51,6 +76,11 @@
       :new-string="newStr"
       :context="context"
       :output-format="outputFormat"
+      :drawFileList="drawFileList"
+      :renderNothingWhenEmpty="renderNothingWhenEmpty"
+      :diffStyle="diffStyle"
+      :fileName="fileName"
+      :isShowNoChange="isShowNoChange"
     />
   </div>
 </template>
@@ -67,7 +97,12 @@ export default {
       oldStr: '',
       newStr: '',
       fotmat: false,
-      context: 10
+      context: 10,
+      diffStyle: 'word',
+      fileName: '',
+      isShowNoChange: true,
+      drawFileList: true,
+      renderNothingWhenEmpty: false
     }
   },
   computed: {
